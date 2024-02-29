aab0d7aa8edb06d9a1a8c09f0d394998798cff36
Bilal Meddah <bilal.meddah@epfl.ch>
2024-02-29 13:14:12 +0100

    154-174/update: add db dependency injection example to a route handler

--- virtual_labs/routes/projects.py
+++ virtual_labs/routes/projects.py
@@ -1,9 +1,18 @@
 from fastapi import APIRouter
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
 
-projects = APIRouter(prefix="/lab/{labid}/projects")
+from ..infrastructure.db.config import default_session_factory
+
+router = APIRouter(prefix="/projects")
 
 
-@projects.get("/{project_id}")
-async def get_project(project_id: str):
-    print(project_id)
-    pass
+"""
+    this is just an example how we should pass the db pool to the route as dependency injection
+    it will be consumed by the service or the use case (if no validation)
+"""
+
+
+@router.get("/{project_id}")
+def retrieve_project(db: Session = Depends(default_session_factory)) -> None:
+    return
