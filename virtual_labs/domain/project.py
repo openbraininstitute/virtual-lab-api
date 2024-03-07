3ef7fc6ecbd1c71f3ec979c504c34cb3bd0dc9a0
Bilal Meddah <bilal.meddah@epfl.ch>
2024-03-07 09:00:07 +0100

    154-194/update: add Project domain models

---
+++ virtual_labs/domain/project.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import UUID4, BaseModel
+
+
+class ProjectCreationModel(BaseModel):
+    name: str
+    description: Optional[str] = None
+    include_members: Optional[list[UUID4]] = None
+
+
+class Project(BaseModel):
+    id: UUID4
+    nexus_project_id: str
+    name: str
+    description: str | None
+    budget: float | None
+    created_at: datetime
+    updated_at: datetime | None
+
+    class Config:
+        orm_mode = True
