d7f60f92c7660c347cb2b7630660afee0f315273
Bilal Meddah <bilal.meddah@epfl.ch>
2024-03-12 17:55:54 +0100

    154-179/update: update nexus errors to enum

--- virtual_labs/core/exceptions/nexus_error.py
+++ virtual_labs/core/exceptions/nexus_error.py
@@ -1,17 +1,7 @@
-from enum import EnumType
+from enum import StrEnum
 
 
-class NexusError(Exception):
-    message: str | None
-    type: str | None
-
-    def __init__(self, *, message: str | None = None, type: str | None = None) -> None:
-        self.message = message
-        self.type = type
-        super().__init__(self.message)
-
-
-class NexusErrorValue(EnumType):
+class NexusErrorValue(StrEnum):
     CREATE_PROJECT = "NEXUS_CREATE_PROJECT"
     CREATE_PROJECT_ACL = "NEXUS_CREATE_PROJECT_ACL"
     CREATE_RESOURCE = "NEXUS_CREATE_RESOURCE"
@@ -22,3 +12,15 @@ class NexusErrorValue(EnumType):
     CREATE_SP_AGG_VIEW = "NEXUS_CREATE_SP_AGG_VIEW"
     FETCH_RESOURCE = "NEXUS_FETCH_RESOURCE"
     GENERIC = "NEXUS_GENERIC"
+
+
+class NexusError(Exception):
+    message: str | None
+    type: NexusErrorValue | None
+
+    def __init__(
+        self, *, message: str | None = None, type: NexusErrorValue | None = None
+    ) -> None:
+        self.message = message
+        self.type = type
+        super().__init__(self.message)
