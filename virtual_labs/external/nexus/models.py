04eaa5f80a46b1a3b34021b8d4adc287e52036ac
Bilal Meddah <bilal.meddah@epfl.ch>
2024-03-13 13:46:44 +0100

    154-179/fix: context inheritance

--- virtual_labs/external/nexus/models.py
+++ virtual_labs/external/nexus/models.py
@@ -1,15 +1,12 @@
 from datetime import datetime
 from typing import Annotated, Any, List, TypedDict
 
-from pydantic import UUID4, AnyUrl, BaseModel, Field
+from pydantic import UUID4, BaseModel, Field
 
 
-class NexusContext:
-    context: Annotated[List[AnyUrl], Field(alias="@context")] = []
-
-
-class NexusBase(BaseModel, NexusContext):
-    id: Annotated[AnyUrl | str, Field(alias="@id")]
+class NexusBase(BaseModel):
+    context: Annotated[List[str], Field(alias="@context")] = []
+    id: Annotated[str, Field(alias="@id")]
     type: Annotated[str | List[str], Field(alias="@type")]
     _createdAt: datetime
     _createdBy: datetime
@@ -43,23 +40,23 @@ class NexusAcls(NexusBase):
 
 
 class NexusAclResult(NexusBase):
-    id: Annotated[AnyUrl | str, Field(alias="@id")] = ""
+    id: Annotated[str, Field(alias="@id")] = ""
     type: Annotated[str | List[str], Field(alias="@type")] = "AccessControlList"
 
 
-class NexusAclIdentity:
-    id: Annotated[AnyUrl | str, Field(alias="@id")]
+class NexusAclIdentity(TypedDict):
+    id: Annotated[str, Field(alias="@id")]
     type: Annotated[str, Field(alias="@type")]
     realm: str
     group: str
 
 
-class NexusAcl:
+class NexusAcl(TypedDict):
     identity: NexusAclIdentity
     permissions: List[str]
 
 
-class NexusAclList(NexusBase, NexusContext):
+class NexusAclList(NexusBase):
     _total: int
     acl: List[NexusAcl]
 
