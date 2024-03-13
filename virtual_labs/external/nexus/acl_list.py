5938b490f54298369ffe4c0afff9668c0d994f36
Bilal Meddah <bilal.meddah@epfl.ch>
2024-03-13 14:42:11 +0100

    154-179/update: fix virtual lab permissions

--- virtual_labs/external/nexus/acl_list.py
+++ virtual_labs/external/nexus/acl_list.py
@@ -8,14 +8,21 @@ project_member_acls: List[str] = [
     "views/write",
     "archives/write",
 ]
+
 project_admin_acls: List[str] = [
     *project_member_acls,
-    "projects/write",
+    "projects/write",  # this will allow deprecation too (which is deletion in our case)
     "projects/read",
+    "projects/delete",  # this will allow strict deletion
 ]
 
 virtual_lab_admin_acls: List[str] = [
     *project_admin_acls,
-    "organizations/read" "organizations/write" "organizations/create",
+    "organizations/read",
+    "organizations/write",
+    "organizations/create",
+    # TODO: we should also request delta to add "organizations/delete"
+    # TODO: also allow deprecation of an organization
 ]
+
 virtual_lab_member_acls: List[str] = []
