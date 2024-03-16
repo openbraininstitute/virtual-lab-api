b18af213a90c9398d231ec3d691b5491c1a0d6c9
Bilal Meddah <bilal.meddah@epfl.ch>
2024-03-16 14:37:48 +0100

    update: user domain

--- virtual_labs/domain/user.py
+++ virtual_labs/domain/user.py
@@ -1,9 +1,21 @@
-from pydantic import UUID4, BaseModel
+from datetime import datetime
+from typing import Annotated, Any
+
+from pydantic import UUID4, BaseModel, Field, field_validator
 
 
 class ShortenedUser(BaseModel):
     id: UUID4
     username: str
+    createdTimestamp: Annotated[datetime, Field(alias="created_at", default="")]
+
+    @field_validator("createdTimestamp", mode="before")
+    @classmethod
+    def convert_timestamp(cls, v: str) -> Any:
+        if isinstance(v, int):
+            return datetime.fromtimestamp(v / 1000)
+        return v
 
     class Config:
         from_attributes = True
+        populate_by_name = True
