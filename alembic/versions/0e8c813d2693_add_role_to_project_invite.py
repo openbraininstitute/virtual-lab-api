d1835d059762adbff438534e39eb338ace339d9c
Dinika Saxena <dinikasaxenas@gmail.com>
2024-03-19 15:58:26 +0100

    Fix alembic revision history

--- alembic/versions/0e8c813d2693_add_role_to_project_invite.py
+++ alembic/versions/0e8c813d2693_add_role_to_project_invite.py
@@ -1,19 +1,19 @@
 """add role to project_invite
 
 Revision ID: 0e8c813d2693
-Revises: c3fc70d340af
+Revises: deac7acdca68
 Create Date: 2024-03-15 13:06:06.643619
 
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "0e8c813d2693"
-down_revision: Union[str, None] = "c3fc70d340af"
+down_revision: Union[str, None] = "deac7acdca68"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
