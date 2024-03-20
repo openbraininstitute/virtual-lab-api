57f215ce2ff4d6e1fa6c71aabd2d5cd0f1d0812f
Bilal Meddah <bilal.meddah@epfl.ch>
2024-03-20 13:44:18 +0100

    update: add include_members list to kc member group and creator to VL group

--- virtual_labs/usecases/project/create_new_project.py
+++ virtual_labs/usecases/project/create_new_project.py
@@ -2,22 +2,25 @@ from http import HTTPStatus as status
 from uuid import uuid4
 
 from fastapi.responses import Response
-from httpx import AsyncClient
+from keycloak import KeycloakError  # type: ignore
 from loguru import logger
 from pydantic import UUID4
 from sqlalchemy.exc import IntegrityError, NoResultFound, SQLAlchemyError
 from sqlalchemy.orm import Session
 
 from virtual_labs.core.exceptions.api_error import VliError, VliErrorCode
+from virtual_labs.core.exceptions.nexus_error import NexusError
 from virtual_labs.core.response.api_response import VliResponse
 from virtual_labs.core.types import UserRoleEnum
 from virtual_labs.domain.project import Project, ProjectCreationBody
+from virtual_labs.external.nexus.project_instance import instantiate_nexus_project
 from virtual_labs.repositories.group_repo import GroupMutationRepository
 from virtual_labs.repositories.labs import get_virtual_lab
 from virtual_labs.repositories.project_repo import (
     ProjectMutationRepository,
     ProjectQueryRepository,
 )
+from virtual_labs.repositories.user_repo import UserMutationRepository
 
 
 async def create_new_project_use_case(
@@ -26,24 +29,26 @@ async def create_new_project_use_case(
     virtual_lab_id: UUID4,
     user_id: UUID4,
     payload: ProjectCreationBody,
-    httpx_clt: AsyncClient,
 ) -> Response | VliError:
-    project_id: UUID4 = uuid4()
     pmr = ProjectMutationRepository(session)
     pqr = ProjectQueryRepository(session)
     gmr = GroupMutationRepository()
+    umr = UserMutationRepository()
+
+    project_id: UUID4 = uuid4()
 
     try:
         get_virtual_lab(session, virtual_lab_id)
+
+        if pqr.retrieve_one_project_by_name(name=payload.name):
+            raise
+
     except NoResultFound:
         raise VliError(
             error_code=VliErrorCode.ENTITY_NOT_FOUND,
             http_status_code=status.BAD_REQUEST,
             message="Virtual lab not found",
         )
-
-    try:
-        pqr.retrieve_one_project_by_name(name=payload.name)
     except Exception as ex:
         logger.error(f"Error during retrieving the project ({ex})")
         raise VliError(
@@ -70,18 +75,37 @@ async def create_new_project_use_case(
         assert admin_group_id is not None
         assert member_group_id is not None
 
+        # TODO: to asyncio need to run in parallel
+        umr.attach_user_to_group(
+            user_id=user_id,
+            group_id=admin_group_id,
+        )
+        if payload.include_members:
+            for member in payload.include_members:
+                umr.attach_user_to_group(
+                    user_id=member,
+                    group_id=member_group_id,
+                )
+
     except AssertionError:
         raise VliError(
             error_code=VliErrorCode.EXTERNAL_SERVICE_ERROR,
             http_status_code=status.BAD_REQUEST,
             message="Admin/Member group_id failed to be generated",
         )
-    except Exception as ex:
-        logger.error(f"Error during creating new group in KC: ({ex})")
+    except KeycloakError as ex:
+        logger.error(f"Error during creating/attaching to group in KC: ({ex})")
         raise VliError(
             error_code=VliErrorCode.EXTERNAL_SERVICE_ERROR,
             http_status_code=status.BAD_REQUEST,
-            message="KC Group creation failed",
+            message="KC Group creation/attaching failed",
+        )
+    except Exception as ex:
+        logger.error(f"Error during creating/attaching to group in KC: ({ex})")
+        raise VliError(
+            error_code=VliErrorCode.EXTERNAL_SERVICE_ERROR,
+            http_status_code=status.BAD_REQUEST,
+            message="KC Group creation/attaching failed",
         )
 
     try:
@@ -92,8 +116,6 @@ async def create_new_project_use_case(
             admin_group_id=admin_group_id,
             member_group_id=member_group_id,
         )
-        # TODO: add include_members list to the group in KC
-
     except NexusError as ex:
         logger.error(f"Error during reverting project instance due nexus error ({ex})")
         gmr.delete_group(group_id=admin_group_id)
@@ -115,10 +137,7 @@ async def create_new_project_use_case(
             admin_group_id=admin_group_id,
             member_group_id=member_group_id,
             owner_id=user_id,
-            # nexus_project_id=nexus_project.id,
         )
-
-        # TODO: add include_members list to the group in KC
     except AssertionError:
         raise VliError(
             error_code=VliErrorCode.EXTERNAL_SERVICE_ERROR,
