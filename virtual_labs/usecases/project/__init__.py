b974e359fe1dc664d6d2ff74f866a337b33e500e
Bilal Meddah <bilal.meddah@epfl.ch>
2024-03-22 16:02:00 +0100

    update : add routes for new use cases

--- virtual_labs/usecases/project/__init__.py
+++ virtual_labs/usecases/project/__init__.py
@@ -1,6 +1,7 @@
 from .check_project_exist import check_project_existence_use_case
 from .create_new_project import create_new_project_use_case
 from .delete_project import delete_project_use_case
+from .detach_user_from_project import detach_user_from_project
 from .retrieve_all_user_projects import (
     retrieve_all_user_projects_use_case,
 )
@@ -20,7 +21,9 @@ from .search_projects_per_lab_by_name import (
     search_projects_per_virtual_lab_by_name_use_case,
 )
 from .update_project_budget import update_project_budget_use_case
+from .update_project_data import update_project_data
 from .update_star_project_status import update_star_project_status_use_case
+from .update_user_role_in_project import update_user_role_in_project
 
 __all__ = [
     "retrieve_all_user_projects_per_vl_use_case",
@@ -38,4 +41,7 @@ __all__ = [
     "delete_project_use_case",
     "update_project_budget_use_case",
     "update_star_project_status_use_case",
+    "update_user_role_in_project",
+    "detach_user_from_project",
+    "update_project_data",
 ]
