c8e193c370016b46b29de5f28a59377fd028bee8
Bilal Meddah <bilal.meddah@epfl.ch>
2024-03-26 18:47:10 +0100

    update: use project repo

--- virtual_labs/repositories/project_repo.py
+++ virtual_labs/repositories/project_repo.py
@@ -4,7 +4,8 @@ from uuid import UUID
 
 from pydantic import UUID4
 from sqlalchemy import Row, delete, func, or_, select, update
-from sqlalchemy.orm import Query, Session, noload
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import noload
 from sqlalchemy.sql import and_
 
 from virtual_labs.core.types import PaginatedDbResult
@@ -14,19 +15,19 @@ from virtual_labs.infrastructure.db.models import Project, ProjectStar, VirtualL
 
 
 class ProjectQueryRepository:
-    session: Session
+    session: AsyncSession
 
-    def __init__(self, session: Session) -> None:
+    def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    def retrieve_projects_per_vl_batch(
+    async def retrieve_projects_per_vl_batch(
         self,
         virtual_lab_id: UUID4,
         groups: List[str],
         pagination: PageParams,
     ) -> PaginatedDbResult[List[Row[Tuple[Project, VirtualLab]]]]:
         query = (
-            self.session.query(Project, VirtualLab)
+            select(Project, VirtualLab)
             .join(VirtualLab)
             .filter(
                 and_(
@@ -39,28 +40,34 @@ class ProjectQueryRepository:
                 )
             )
         )
-        count = self.session.scalar(
+        count = await self.session.scalar(
             select(func.count()).select_from(query.options(noload("*")).subquery())
         )
         result = (
-            query.order_by(Project.updated_at)
-            .offset((pagination.page - 1) * pagination.size)
-            .limit(pagination.size)
-            .all()
-        )
+            await self.session.execute(
+                statement=query.order_by(Project.updated_at)
+                .offset((pagination.page - 1) * pagination.size)
+                .limit(pagination.size)
+            )
+        ).all()
 
         return PaginatedDbResult(
+<<<<<<< HEAD
             count=count or 0,
             rows=result,
+=======
+            count=count,
+            rows=[row for row in result],
+>>>>>>> 9933135 (update: use project repo)
         )
 
-    def retrieve_projects_batch(
+    async def retrieve_projects_batch(
         self,
         groups: List[str],
         pagination: PageParams,
     ) -> PaginatedDbResult[List[Row[Tuple[Project, VirtualLab]]]]:
         query = (
-            self.session.query(Project, VirtualLab)
+            select(Project, VirtualLab)
             .join(VirtualLab)
             .filter(
                 and_(
@@ -72,35 +79,41 @@ class ProjectQueryRepository:
                 )
             )
         )
-        count = self.session.scalar(
+
+        count = await self.session.scalar(
             select(func.count()).select_from(query.options(noload("*")).subquery())
         )
+
         result = (
-            query.order_by(Project.updated_at)
-            .offset((pagination.page - 1) * pagination.size)
-            .limit(pagination.size)
-            .all()
-        )
+            await self.session.execute(
+                statement=query.order_by(Project.updated_at)
+                .offset((pagination.page - 1) * pagination.size)
+                .limit(pagination.size)
+            )
+        ).all()
 
         return PaginatedDbResult(
             count=count or 0,
-            rows=result,
+            rows=[row for row in result],
         )
 
-    def retrieve_one_project_strict(
+    async def retrieve_one_project_strict(
         self, virtual_lab_id: UUID4, project_id: UUID4
     ) -> Tuple[Project, VirtualLab]:
         stmt = (
             select(Project, VirtualLab)
             .join(VirtualLab)
             .filter(
-                and_(Project.id == project_id, Project.virtual_lab_id == virtual_lab_id)
+                and_(
+                    Project.id == project_id,
+                    Project.virtual_lab_id == virtual_lab_id,
+                )
             )
         )
-        result = self.session.execute(statement=stmt)
+        result = await self.session.execute(statement=stmt)
         return cast(Tuple[Project, VirtualLab], result.one())
 
-    def retrieve_one_project(
+    async def retrieve_one_project(
         self, virtual_lab_id: UUID4, project_id: UUID4
     ) -> Tuple[Project, VirtualLab] | None:
         stmt = (
@@ -110,10 +123,10 @@ class ProjectQueryRepository:
                 and_(Project.id == project_id, Project.virtual_lab_id == virtual_lab_id)
             )
         )
-        result = self.session.execute(statement=stmt)
-        return cast(Tuple[Project, VirtualLab], result.first())
+        result = (await self.session.execute(statement=stmt)).first()
+        return cast(Tuple[Project, VirtualLab] | None, result)
 
-    def retrieve_one_project_by_id(
+    async def retrieve_one_project_by_id(
         self, project_id: UUID4
     ) -> Tuple[Project, VirtualLab]:
         stmt = (
@@ -121,30 +134,39 @@ class ProjectQueryRepository:
             .join(VirtualLab)
             .filter(and_(Project.id == project_id))
         )
-        result = self.session.execute(statement=stmt)
+        result = await self.session.execute(statement=stmt)
         return cast(Tuple[Project, VirtualLab], result.one())
 
-    def retrieve_one_project_by_name(self, name: str) -> Project | None:
-        return self.session.query(Project).filter(Project.name == name).first()
-
-    def retrieve_project_star(
-        self, *, project_id: UUID4, user_id: UUID4
-    ) -> ProjectStar | None:
-        return (
-            self.session.query(ProjectStar)
-            .filter(
-                and_(
-                    ProjectStar.project_id == project_id, ProjectStar.user_id == user_id
-                )
-            )
-            .first()
+    async def retrieve_one_project_by_name(self, name: str) -> Project | None:
+        result = await self.session.scalar(
+            select(Project).filter(Project.name == name),
+        )
+        return cast(
+            Project,
+            result,
         )
 
-    def retrieve_starred_projects_per_user(
-        self, user_id: UUID4
-    ) -> List[Row[Tuple[ProjectStar, Project]]]:
-        joined_query = (
-            self.session.query(ProjectStar, Project)
+    async def retrieve_project_star(
+        self, *, project_id: UUID4, user_id: UUID4
+    ) -> ProjectStar | None:
+        result = await self.session.scalar(
+            select(ProjectStar).filter(
+                and_(
+                    ProjectStar.project_id == project_id,
+                    ProjectStar.user_id == user_id,
+                )
+            )
+        )
+        return cast(
+            ProjectStar,
+            result,
+        )
+
+    async def retrieve_starred_projects_per_user(
+        self, user_id: UUID4, pagination: PageParams
+    ) -> PaginatedDbResult[List[Row[Tuple[ProjectStar, Project]]]]:
+        query = (
+            select(ProjectStar, Project)
             .join(Project, ProjectStar.project_id == Project.id)
             .filter(
                 and_(
@@ -153,24 +175,47 @@ class ProjectQueryRepository:
                 )
             )
         )
-        user_starred_projects = joined_query.all()
-        return user_starred_projects
-
-    def retrieve_project_users_count(self, virtual_lab_id: UUID4) -> int:
-        return (
-            self.session.query(Project)
-            .filter(Project.virtual_lab_id == virtual_lab_id)
-            .count()
+        count = await self.session.scalar(
+            select(func.count()).select_from(query.options(noload("*")).subquery())
         )
 
-    def retrieve_projects_per_lab_count(self, virtual_lab_id: UUID4) -> int:
-        return (
-            self.session.query(Project)
-            .filter(and_(~Project.deleted, Project.virtual_lab_id == virtual_lab_id))
-            .count()
+        result = (
+            await self.session.execute(
+                statement=query.order_by(Project.updated_at)
+                .offset((pagination.page - 1) * pagination.size)
+                .limit(pagination.size)
+            )
+        ).all()
+
+        return PaginatedDbResult(
+            count=count,
+            rows=[row for row in result],
         )
 
-    def search(
+    async def retrieve_project_users_count(self, virtual_lab_id: UUID4) -> int | None:
+        result = await self.session.execute(
+            select(func.count(Project.id)).where(  # Use select for flexibility
+                Project.virtual_lab_id == virtual_lab_id
+            )
+        )
+
+        return result.scalar()
+
+    async def retrieve_projects_per_lab_count(
+        self, virtual_lab_id: UUID4
+    ) -> int | None:
+        result = await self.session.scalar(
+            select(func.count(Project.id)).where(
+                and_(~Project.deleted, Project.virtual_lab_id == virtual_lab_id)
+            )
+        )
+
+        return cast(
+            int,
+            result,
+        )
+
+    async def search(
         self,
         *,
         query_term: str,
@@ -210,23 +255,32 @@ class ProjectQueryRepository:
             .filter(*conditions)
             .order_by(Project.updated_at)
         )
-        result = self.session.execute(statement=stmt)
-        return cast(List[Tuple[Project, VirtualLab]], result.all())
+        result = (await self.session.execute(statement=stmt)).all()
 
-    def check(self, *, query_term: str) -> Query[Project]:
-        query = self.session.query(Project).filter(
-            func.lower(Project.name) == func.lower(query_term)
+        return cast(
+            List[Tuple[Project, VirtualLab]],
+            result,
         )
-        return query
+
+    async def check_project_exists_by_name(self, *, query_term: str) -> int | None:
+        count = await self.session.scalar(
+            select(func.count(Project.id)).filter(
+                func.lower(Project.name) == func.lower(query_term)
+            )
+        )
+        return cast(
+            int,
+            count,
+        )
 
 
 class ProjectMutationRepository:
-    session: Session
+    session: AsyncSession
 
-    def __init__(self, session: Session) -> None:
+    def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    def create_new_project(
+    async def create_new_project(
         self,
         *,
         payload: ProjectCreationBody,
@@ -248,11 +302,11 @@ class ProjectMutationRepository:
             owner_id=owner_id,
         )
         self.session.add(project)
-        self.session.commit()
-        self.session.refresh(project)
+        await self.session.commit()
+        await self.session.refresh(project)
         return project
 
-    def un_delete_project(
+    async def un_delete_project(
         self, *, virtual_lab_id: UUID4, project_id: UUID4
     ) -> Row[Tuple[UUID, str, str, bool, datetime]]:
         stmt = (
@@ -269,11 +323,11 @@ class ProjectMutationRepository:
                 Project.deleted_at,
             )
         )
-        result = self.session.execute(statement=stmt)
-        self.session.commit()
+        result = await self.session.execute(statement=stmt)
+        await self.session.commit()
         return result.one()
 
-    def delete_project(
+    async def delete_project(
         self, virtual_lab_id: UUID4, project_id: UUID4, user_id: UUID4
     ) -> Row[Tuple[UUID, bool, datetime]]:
         stmt = (
@@ -288,11 +342,11 @@ class ProjectMutationRepository:
                 Project.deleted_at,
             )
         )
-        result = self.session.execute(statement=stmt)
-        self.session.commit()
+        result = await self.session.execute(statement=stmt)
+        await self.session.commit()
         return result.one()
 
-    def delete_project_strict(
+    async def delete_project_strict(
         self, virtual_lab_id: UUID4, project_id: UUID4
     ) -> Row[Tuple[UUID, str, str, bool, datetime]]:
         stmt = (
@@ -308,11 +362,11 @@ class ProjectMutationRepository:
                 Project.deleted_at,
             )
         )
-        result = self.session.execute(statement=stmt)
-        self.session.commit()
+        result = await self.session.execute(statement=stmt)
+        await self.session.commit()
         return result.one()
 
-    def update_project_budget(
+    async def update_project_budget(
         self, virtual_lab_id: UUID4, project_id: UUID4, value: float
     ) -> Row[Tuple[UUID, Any, datetime]]:
         stmt = (
@@ -323,11 +377,11 @@ class ProjectMutationRepository:
             .values(budget=value)
             .returning(Project.id, Project.budget, Project.updated_at)
         )
-        result = self.session.execute(statement=stmt)
-        self.session.commit()
+        result = await self.session.execute(statement=stmt)
+        await self.session.commit()
         return result.one()
 
-    def update_project_nexus_id(
+    async def update_project_nexus_id(
         self, virtual_lab_id: UUID4, project_id: UUID4, nexus_id: str
     ) -> Row[Tuple[UUID, str, datetime]]:
         stmt = (
@@ -338,21 +392,21 @@ class ProjectMutationRepository:
             .values(nexus_project_id=nexus_id)
             .returning(Project.id, Project.nexus_project_id, Project.updated_at)
         )
-        result = self.session.execute(statement=stmt)
-        self.session.commit()
+        result = await self.session.execute(statement=stmt)
+        await self.session.commit()
         return result.one()
 
-    def star_project(self, user_id: UUID4, project_id: UUID4) -> ProjectStar:
+    async def star_project(self, user_id: UUID4, project_id: UUID4) -> ProjectStar:
         project = ProjectStar(
             project_id=project_id,
             user_id=user_id,
         )
         self.session.add(project)
-        self.session.commit()
-        self.session.refresh(project)
+        await self.session.commit()
+        await self.session.refresh(project)
         return project
 
-    def unstar_project(
+    async def unstar_project(
         self, *, project_id: UUID4, user_id: UUID4
     ) -> Row[Tuple[UUID, datetime]]:
         stmt = (
@@ -364,11 +418,11 @@ class ProjectMutationRepository:
             )
             .returning(ProjectStar.project_id, ProjectStar.updated_at)
         )
-        result = self.session.execute(statement=stmt)
-        self.session.commit()
+        result = await self.session.execute(statement=stmt)
+        await self.session.commit()
         return result.one()
 
-    def update_project_data(
+    async def update_project_data(
         self, virtual_lab_id: UUID4, project_id: UUID4, payload: ProjectBody
     ) -> Row[Tuple[Project]]:
         stmt = (
@@ -384,7 +438,7 @@ class ProjectMutationRepository:
             )
             .returning(Project)
         )
-        result = self.session.execute(statement=stmt)
-        self.session.commit()
+        result = await self.session.execute(statement=stmt)
+        await self.session.commit()
 
         return result.one()
