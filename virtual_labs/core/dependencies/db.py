ae24fd6ef5251b8f0a578a8c32615943e2e56fce
Bilal Meddah <bilal.meddah@epfl.ch>
2024-03-27 09:42:53 +0100

    clean: remove dependencies folder

--- virtual_labs/core/dependencies/db.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from virtual_labs.infrastructure.db.config import default_session_factory
-
-DBSessionDependency = Annotated[AsyncSession, Depends(default_session_factory)]
