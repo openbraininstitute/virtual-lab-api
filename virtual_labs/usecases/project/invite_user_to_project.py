eade4e2338a2e02f1cfe12dabcdeeab0e24c2c02
Bilal Meddah <bilal.meddah@epfl.ch>
2024-04-03 12:13:05 +0200

    update: fix invite project, do not create another row if the invite already exists

--- virtual_labs/usecases/project/invite_user_to_project.py
+++ virtual_labs/usecases/project/invite_user_to_project.py
@@ -1,8 +1,10 @@
 from http import HTTPStatus
+from typing import Tuple
 from uuid import UUID
 
 from loguru import logger
 from pydantic import UUID4
+from sqlalchemy import func
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 
@@ -11,52 +13,81 @@ from virtual_labs.core.exceptions.email_error import EmailError
 from virtual_labs.core.types import VliAppResponse
 from virtual_labs.domain.project import ProjectInviteIn, ProjectInviteOut
 from virtual_labs.infrastructure.email.email_service import EmailDetails, send_invite
+from virtual_labs.infrastructure.kc.models import AuthUser
 from virtual_labs.repositories import labs as lab_repo
-from virtual_labs.repositories.invite_repo import InviteMutationRepository
+from virtual_labs.repositories.invite_repo import (
+    InviteMutationRepository,
+    InviteQueryRepository,
+)
 from virtual_labs.repositories.project_repo import ProjectQueryRepository
 from virtual_labs.repositories.user_repo import UserQueryRepository
+from virtual_labs.shared.utils.auth import get_user_id_from_auth
 
 
 async def invite_user_to_project(
-    lab_id: UUID4,
-    project_id: UUID4,
-    inviter_id: UUID4,
-    invite_details: ProjectInviteIn,
     session: Session,
+    *,
+    virtual_lab_id: UUID4,
+    project_id: UUID4,
+    invite_details: ProjectInviteIn,
+    auth: Tuple[AuthUser, str],
 ) -> VliAppResponse[ProjectInviteOut]:
     pr = ProjectQueryRepository(session)
     user_repo = UserQueryRepository()
     invite_repo = InviteMutationRepository(session)
+    invite_query_repo = InviteQueryRepository(session)
+    inviter_id = get_user_id_from_auth(auth)
 
     try:
         project, _ = pr.retrieve_one_project_strict(
-            virtual_lab_id=lab_id, project_id=project_id
+            virtual_lab_id=virtual_lab_id, project_id=project_id
         )
-        lab = lab_repo.get_virtual_lab(session, lab_id)
+        lab = lab_repo.get_virtual_lab(session, virtual_lab_id)
 
-        user_to_invite = user_repo.retrieve_user_by_email(invite_details.email)
-        user_id = UUID(user_to_invite.id) if user_to_invite is not None else None
-
-        invite = invite_repo.add_project_invite(
+        invite = invite_query_repo.get_project_invite_by_params(
             project_id=project_id,
-            # Inviter details
-            inviter_id=inviter_id,
-            # Invitee details
-            invitee_id=user_id,
-            invitee_role=invite_details.role,
-            invitee_email=invite_details.email,
+            email=invite_details.email,
+            role=invite_details.role,
         )
 
-        await send_invite(
-            details=EmailDetails(
-                recipient=invite_details.email,
-                invite_id=UUID(str(invite.id)),
-                lab_id=lab_id,
-                lab_name=str(lab.name),
+        if invite is None:
+            user_to_invite = user_repo.retrieve_user_by_email(invite_details.email)
+            user_id = UUID(user_to_invite.id) if user_to_invite is not None else None
+
+            invite = invite_repo.add_project_invite(
                 project_id=project_id,
-                project_name=str(project.name),
+                inviter_id=inviter_id,
+                invitee_id=user_id,
+                invitee_role=invite_details.role,
+                invitee_email=invite_details.email,
             )
-        )
+
+            await send_invite(
+                details=EmailDetails(
+                    recipient=invite_details.email,
+                    invite_id=UUID(str(invite.id)),
+                    lab_id=virtual_lab_id,
+                    lab_name=str(lab.name),
+                    project_id=project_id,
+                    project_name=str(project.name),
+                )
+            )
+        else:
+            invite_repo.update_project_invite(
+                invite_id=invite.id,
+                properties={"updated_at": func.now()},
+            )
+            await send_invite(
+                details=EmailDetails(
+                    recipient=invite_details.email,
+                    invite_id=UUID(str(invite.id)),
+                    lab_id=virtual_lab_id,
+                    lab_name=str(lab.name),
+                    project_id=project_id,
+                    project_name=str(project.name),
+                )
+            )
+
         return VliAppResponse[ProjectInviteOut](
             message="Invite sent to user",
             data=ProjectInviteOut(invite_id=UUID(str(invite.id))),
