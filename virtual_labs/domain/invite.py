c0f8c6e9772f07833d146869f069c450146e690b
Bilal Meddah <bilal.meddah@epfl.ch>
2024-04-03 21:44:15 +0200

    update: send vlab/project id in response

---
+++ virtual_labs/domain/invite.py
@@ -0,0 +1,10 @@
+from pydantic import UUID4, BaseModel
+
+from virtual_labs.infrastructure.email.email_utils import InviteOrigin
+
+
+class InviteOut(BaseModel):
+    invite_id: UUID4
+    virtual_lab_id: UUID4
+    project_id: UUID4 | None
+    origin: InviteOrigin
