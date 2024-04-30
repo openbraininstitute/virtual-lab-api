519dcc23910161ad43e114dd2e5ab72ba7bbb2ec
Dinika Saxena <dinikasaxenas@gmail.com>
2024-04-30 16:29:28 +0200

    Remove load_env since env files are being loaded by priority later
    Signed-off-by: Dinika Saxena <dinikasaxenas@gmail.com>

--- virtual_labs/infrastructure/settings.py
+++ virtual_labs/infrastructure/settings.py
@@ -1,11 +1,8 @@
 from typing import Any, Literal, Optional
 
-from dotenv import load_dotenv
 from pydantic import EmailStr, PostgresDsn, ValidationInfo, field_validator
 from pydantic_core import MultiHostUrl, Url
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-load_dotenv("")
 
 
 class Settings(BaseSettings):
