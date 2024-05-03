770fb904f37f35746eb09d19e8ceec8ba880564a
Bilal Meddah <bilal.meddah@epfl.ch>
2024-05-03 15:06:37 +0200

    update: add new payment method and list pms usecases

---
+++ virtual_labs/usecases/billing/__init__.py
@@ -0,0 +1,7 @@
+from .attach_payment_method_to_vl import attach_payment_method_to_virtual_lab
+from .retrieve_vl_payment_methods import retrieve_virtual_lab_payment_methods
+
+__all__ = [
+    "retrieve_virtual_lab_payment_methods",
+    "attach_payment_method_to_virtual_lab",
+]
