5aa00b6483d940c6d45861be144b2f4121e70917
Bilal Meddah <bilal.meddah@epfl.ch>
2024-05-14 09:29:16 +0200

    update: add test delete payment method

--- virtual_labs/infrastructure/stripe/config.py
+++ virtual_labs/infrastructure/stripe/config.py
@@ -2,8 +2,6 @@ import stripe
 
 from virtual_labs.infrastructure.settings import settings
 
-stripe.api_key = settings.STRIPE_SECRET_KEY
-
 stripe_client = stripe.StripeClient(
     settings.STRIPE_SECRET_KEY,
 )
