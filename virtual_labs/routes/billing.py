52e40f6a8e15d030c4fba26ff1b52f5a0f0b4818
Bilal Meddah <bilal.meddah@epfl.ch>
2024-05-20 17:52:57 +0200

    update: add routes for start_payment/get_balance

--- virtual_labs/routes/billing.py
+++ virtual_labs/routes/billing.py
@@ -16,6 +16,8 @@ from virtual_labs.domain.payment_method import (
     PaymentMethodOut,
     PaymentMethodsOut,
     SetupIntentOut,
+    StripePaymentOut,
+    VlabBalanceOut,
 )
 from virtual_labs.infrastructure.db.config import default_session_factory
 from virtual_labs.infrastructure.kc.auth import verify_jwt
@@ -67,25 +69,6 @@ async def add_new_payment_method_to_vl(
     )
 
 
-@router.post(
-    "/{virtual_lab_id}/billing/setup-intent",
-    operation_id="generate_setup_intent",
-    summary="generate setup intent for a specific stripe customer (customer == virtual lab)",
-    response_model=VliAppResponse[SetupIntentOut],
-)
-@verify_vlab_write
-async def generate_setup_intent(
-    virtual_lab_id: UUID4,
-    session: AsyncSession = Depends(default_session_factory),
-    auth: Tuple[AuthUser, str] = Depends(verify_jwt),
-) -> Response:
-    return await billing_cases.generate_setup_intent(
-        session,
-        virtual_lab_id=virtual_lab_id,
-        auth=auth,
-    )
-
-
 @router.patch(
     "/{virtual_lab_id}/billing/payment-methods/default",
     operation_id="update_default_payment_method",
@@ -129,4 +112,68 @@ async def delete_payment_method(
         virtual_lab_id=virtual_lab_id,
         payment_method_id=payment_method_id,
         auth=auth,
+    )
+
+
+@router.post(
+    "/{virtual_lab_id}/billing/setup-intent",
+    operation_id="generate_setup_intent",
+    summary="generate setup intent for a specific stripe customer (customer == virtual lab)",
+    response_model=VliAppResponse[SetupIntentOut],
+)
+@verify_vlab_write
+async def generate_setup_intent(
+    virtual_lab_id: UUID4,
+    session: AsyncSession = Depends(default_session_factory),
+    auth: Tuple[AuthUser, str] = Depends(verify_jwt),
+) -> Response:
+    return await billing_cases.generate_setup_intent(
+        session,
+        virtual_lab_id=virtual_lab_id,
+        auth=auth,
+    )
+
+
+@router.post(
+    "/{virtual_lab_id}/billing/budget-topup",
+    operation_id="init_vl_budget_topup",
+    summary="""
+    init virtual lab adding new budget amount processing
+    """,
+    response_model=VliAppResponse[StripePaymentOut],
+)
+@verify_vlab_write
+async def init_vl_budget_topup(
+    virtual_lab_id: UUID4,
+    payment_method_id: Annotated[UUID4, Body(embed=True)],
+    credit: Annotated[float, Body(embed=True)],
+    session: AsyncSession = Depends(default_session_factory),
+    auth: Tuple[AuthUser, str] = Depends(verify_jwt),
+) -> Response:
+    return await billing_cases.init_vl_budget_topup(
+        session,
+        virtual_lab_id=virtual_lab_id,
+        payment_method_id=payment_method_id,
+        credit=credit,
+        auth=auth,
+    )
+
+
+@router.get(
+    "/{virtual_lab_id}/billing/balance",
+    operation_id="retrieve_virtual_lab_balance",
+    summary="""
+    retrieve current balance (budget, total_spent) for a virtual lab
+    """,
+    response_model=VliAppResponse[VlabBalanceOut],
+)
+@verify_vlab_read
+async def retrieve_virtual_lab_balance(
+    virtual_lab_id: UUID4,
+    session: AsyncSession = Depends(default_session_factory),
+    auth: Tuple[AuthUser, str] = Depends(verify_jwt),
+) -> Response:
+    return await billing_cases.retrieve_virtual_lab_balance(
+        session,
+        virtual_lab_id=virtual_lab_id,
     )
