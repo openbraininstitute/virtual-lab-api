b2815e043bc5317805034f849114d5193bd13ee0
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-01-21 14:42:15 +0100

    Add missing dependencies for authz checks

--- virtual_labs/routes/accounting.py
+++ virtual_labs/routes/accounting.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
+from typing import Tuple
+
+from fastapi import APIRouter, Depends
 from pydantic import UUID4
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from virtual_labs.core.authorization import verify_project_read, verify_vlab_read
 from virtual_labs.external.accounting import balance as accounting_balance
@@ -10,6 +13,9 @@ from virtual_labs.external.accounting.models import (
     VirtualLabReportsResponse,
     VlabBalanceResponse,
 )
+from virtual_labs.infrastructure.db.config import default_session_factory
+from virtual_labs.infrastructure.kc.auth import verify_jwt
+from virtual_labs.infrastructure.kc.models import AuthUser
 
 router = APIRouter(
     prefix="/virtual-labs",
@@ -28,7 +34,10 @@ router = APIRouter(
 )
 @verify_vlab_read
 async def get_vl_account_balance(
-    virtual_lab_id: UUID4, include_projects: bool = False
+    virtual_lab_id: UUID4,
+    include_projects: bool = False,
+    session: AsyncSession = Depends(default_session_factory),
+    auth: Tuple[AuthUser, str] = Depends(verify_jwt),
 ) -> VlabBalanceResponse:
     return await accounting_balance.get_virtual_lab_balance(
         virtual_lab_id, include_projects
@@ -44,6 +53,8 @@ async def get_vl_account_balance(
 @verify_project_read
 async def get_proj_account_balance(
     project_id: UUID4,
+    session: AsyncSession = Depends(default_session_factory),
+    auth: Tuple[AuthUser, str] = Depends(verify_jwt),
 ) -> ProjBalanceResponse:
     return await accounting_balance.get_project_balance(project_id)
 
@@ -59,7 +70,11 @@ async def get_proj_account_balance(
 )
 @verify_vlab_read
 async def get_vl_accounting_reports(
-    virtual_lab_id: UUID4, page: int, page_size: int
+    virtual_lab_id: UUID4,
+    page: int,
+    page_size: int,
+    session: AsyncSession = Depends(default_session_factory),
+    auth: Tuple[AuthUser, str] = Depends(verify_jwt),
 ) -> VirtualLabReportsResponse:
     return await accounting_report.get_virtual_lab_reports(
         virtual_lab_id, page, page_size
@@ -74,6 +89,10 @@ async def get_vl_accounting_reports(
 )
 @verify_project_read
 async def get_proj_accounting_reports(
-    project_id: UUID4, page: int, page_size: int
+    project_id: UUID4,
+    page: int,
+    page_size: int,
+    session: AsyncSession = Depends(default_session_factory),
+    auth: Tuple[AuthUser, str] = Depends(verify_jwt),
 ) -> ProjectReportsResponse:
     return await accounting_report.get_project_reports(project_id, page, page_size)
