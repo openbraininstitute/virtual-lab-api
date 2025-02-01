d3285e870ee01e9503412ebbf5b3709d75eae33a
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-02-02 00:08:28 +0100

    Modularize

--- virtual_labs/routes/notebooks.py
+++ virtual_labs/routes/notebooks.py
@@ -2,7 +2,6 @@ from typing import Annotated, TypeVar
 from uuid import UUID
 
 from fastapi import APIRouter, Depends
-from sqlalchemy import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
@@ -13,8 +12,10 @@ from virtual_labs.domain.common import PaginatedResultsResponse
 from virtual_labs.domain.notebooks import Notebook as NotebookResult
 from virtual_labs.domain.notebooks import NotebookCreate
 from virtual_labs.infrastructure.db.config import default_session_factory
-from virtual_labs.infrastructure.db.models import Notebook
-from virtual_labs.usecases.notebooks import get_notebooks_usecase
+from virtual_labs.usecases.notebooks import (
+    create_notebook_usecase,
+    get_notebooks_usecase,
+)
 
 router = APIRouter(prefix="/projects/{project_id}/notebooks", tags=["Notebooks"])
 
@@ -22,7 +23,7 @@ router = APIRouter(prefix="/projects/{project_id}/notebooks", tags=["Notebooks"]
 M = TypeVar("M", bound=DeclarativeBase)
 
 
-@router.get("/")
+@router.get("/", response_model=None)
 async def list_notebooks(
     paginator: QueryPaginator = Depends(),
     auth_project_id: UUID = Depends(verify_vlab_or_project_read_dep),
@@ -31,20 +32,12 @@ async def list_notebooks(
     return VliAppResponse(message="Found!", data=notebooks)
 
 
-@router.post("/")
+@router.post("/", response_model=None)
 async def create_notebook(
     create_notebook: NotebookCreate,
     session: Annotated[AsyncSession, Depends(default_session_factory)],
     auth_project_id: UUID = Depends(verify_vlab_or_project_read_dep),
 ) -> VliAppResponse[NotebookResult]:
-    stmt = (
-        insert(Notebook)
-        .values(**create_notebook.model_dump(), project_id=auth_project_id)
-        .returning(Notebook)
-    )
+    res = await create_notebook_usecase(auth_project_id, create_notebook, session)
 
-    res = (await session.execute(stmt)).scalar_one()
-
-    return VliAppResponse(
-        message="Notebook created successfully", data=NotebookResult.model_validate(res)
-    )
+    return VliAppResponse(message="Notebook created successfully", data=res)
