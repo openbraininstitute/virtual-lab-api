36aa425eaa68c6d0a8accf625e7236a3234bfbe5
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-02-01 22:58:59 +0100

    working

---
+++ virtual_labs/core/pagination.py
@@ -0,0 +1,56 @@
+from typing import Annotated, Generic, TypeVar
+
+from fastapi import Depends, Query
+from pydantic import BaseModel
+from sqlalchemy import func
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.sql.selectable import Select
+
+from virtual_labs.domain.common import (
+    PaginatedResultsResponse,
+)
+from virtual_labs.infrastructure.db.config import default_session_factory
+
+M = TypeVar("M", bound=DeclarativeBase)
+
+
+class QueryPaginator(Generic[M]):
+    def __init__(
+        self,
+        session: Annotated[AsyncSession, Depends(default_session_factory)],
+        page: Annotated[
+            int, Query(ge=1, description="Page number, starting from 1")
+        ] = 1,
+        size: Annotated[
+            int,
+            Query(ge=1, le=100, description="Number of items per page (1-100)"),
+        ] = 50,
+    ):
+        self.page = page
+        self.size = size
+        self.session = session
+
+    def total_query(self, query: Select[tuple[M]]) -> Select[tuple[int]]:
+        return query.with_only_columns(func.coalesce(func.count(), 0)).order_by(None)
+
+    def paginate_query(self, query: Select[tuple[M]]) -> Select[tuple[M]]:
+        return query.offset((self.page - 1) * self.size).limit(self.size)
+
+    async def get_paginated_results(
+        self, query: Select[tuple[M]]
+    ) -> PaginatedResultsResponse[BaseModel]:
+        paginated_query = self.paginate_query(query)
+
+        total_query = self.total_query(query)
+        total_result = await self.session.execute(total_query)
+        result = await self.session.execute(paginated_query)
+        notebooks = list(result.scalars().all())
+        return (  # type: ignore
+            PaginatedResultsResponse(
+                total=total_result.scalar() or 0,
+                page=self.page,
+                page_size=len(notebooks),
+                results=notebooks,
+            ),
+        )
---
+++ virtual_labs/usecases/notebooks.py
@@ -0,0 +1,39 @@
+from typing import Annotated, cast
+from uuid import UUID
+
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from virtual_labs.core.pagination import QueryPaginator
+from virtual_labs.domain.common import (
+    PaginatedResultsResponse,
+)
+from virtual_labs.domain.notebooks import Notebook as NotebookResult
+from virtual_labs.domain.notebooks import NotebookCreate
+from virtual_labs.infrastructure.db.config import default_session_factory
+from virtual_labs.infrastructure.db.models import Notebook
+
+
+async def get_notebooks_usecase(
+    project_id: UUID, query_paginator: QueryPaginator[Notebook]
+) -> PaginatedResultsResponse[NotebookResult]:
+    query = select(Notebook).where(Notebook.project_id == project_id)
+    return cast(
+        PaginatedResultsResponse[NotebookResult],
+        await query_paginator.get_paginated_results(query),
+    )
+
+
+async def create_notebook_usecase(
+    project_id: UUID,
+    notebook_create: NotebookCreate,
+    session: Annotated[AsyncSession, Depends(default_session_factory)],
+) -> NotebookResult:
+    notebook = Notebook(**notebook_create.model_dump(), project_id=project_id)
+
+    session.add(notebook)
+    await session.commit()
+    await session.refresh(notebook)
+
+    return NotebookResult.model_validate(notebook)
