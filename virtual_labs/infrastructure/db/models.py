45a551d5866f0d9dba3e5265f4ab20315045b61c
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-02-01 08:18:14 +0100

    Update Notebook created_at for consistency

--- virtual_labs/infrastructure/db/models.py
+++ virtual_labs/infrastructure/db/models.py
@@ -266,7 +266,9 @@ class Notebook(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
-    created_at: Mapped[datetime] = mapped_column(default=func.now(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
     )
