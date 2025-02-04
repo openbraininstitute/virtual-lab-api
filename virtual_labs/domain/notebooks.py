6b6ef35689aa5ec1bec258f1f706d71dc2f219f0
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2025-02-04 23:19:32 +0100

    More concise

--- virtual_labs/domain/notebooks.py
+++ virtual_labs/domain/notebooks.py
@@ -2,20 +2,20 @@ from datetime import datetime
 from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel, UrlConstraints, field_validator
+from pydantic import BaseModel, TypeAdapter, UrlConstraints, field_validator
 from pydantic_core import Url
 
 
 class NotebookCreate(BaseModel):
     github_file_url: str
 
-    @field_validator("github_file_url", mode="before")
+    @field_validator("github_file_url")
     @classmethod
     def validate_url(cls, value: str) -> str:
         HttpsUrl = Annotated[
             Url, UrlConstraints(max_length=2083, allowed_schemes=["https"])
         ]
-        return str(HttpsUrl(value))
+        return str(TypeAdapter(HttpsUrl).validate_strings(value))
 
 
 class Notebook(BaseModel):
