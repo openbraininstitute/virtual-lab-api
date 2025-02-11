b892667e8fd6bd7783142eabf723e4bfe3b96f7e
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-02-11 10:18:30 +0100

    Remove pro_id prop from project job report model

--- virtual_labs/tests/accounting/test_report_interface.py
+++ virtual_labs/tests/accounting/test_report_interface.py
@@ -115,7 +115,6 @@ async def test_get_project_reports_success(
             "items": [
                 {
                     "job_id": str(uuid4()),
-                    "proj_id": str(uuid4()),
                     "type": "oneshot",
                     "subtype": "ml-llm",
                     "reserved_at": "2025-01-10T10:12:03Z",
