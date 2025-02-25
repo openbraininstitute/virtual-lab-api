a1e2c7cfc97336c44456f08c2c2cc08fecde9ebe
bilalesi <b_meddah@esi.dz>
2025-02-25 17:37:47 +0100

    remove unnecessary test

--- virtual_labs/tests/projects/test_project_creation.py
+++ virtual_labs/tests/projects/test_project_creation.py
@@ -1,16 +1,14 @@
 from typing import AsyncGenerator
 from uuid import uuid4
 
-import httpx
 import pytest
 import pytest_asyncio
 from httpx import AsyncClient, Response
 from requests import get
 
-from virtual_labs.external.nexus.defaults import AGGREGATE_ELASTIC_SEARCH_VIEW
 from virtual_labs.infrastructure.settings import settings
 from virtual_labs.repositories.group_repo import GroupQueryRepository
-from virtual_labs.tests.utils import get_client_headers, wait_until
+from virtual_labs.tests.utils import get_client_headers
 
 
 @pytest.mark.asyncio
@@ -126,49 +124,3 @@ async def test_vlm_project_with_members_creation(
     assert response.status_code == 200
     failed_invites = response.json()["data"]["failed_invites"]
     assert failed_invites == []
-
-
-@pytest.mark.asyncio
-async def test_aggregate_views_created_on_project_creation(
-    mock_create_project: tuple[Response, dict[str, str], dict[str, str]],
-) -> None:
-    (response, headers, payload) = mock_create_project
-
-    project_id = response.json()["data"]["project"]["id"]
-
-    print("ᦨ #  test_project_creation.py:139 #  project_id:", project_id)
-
-    virtual_lab_id = response.json()["data"]["project"]["virtual_lab_id"]
-
-    print("ᦨ #  test_project_creation.py:143 #  virtual_lab_id:", virtual_lab_id)
-
-    async def views_created() -> bool:
-        async with httpx.AsyncClient() as client:
-            nexus_views = await client.get(
-                f"{settings.NEXUS_DELTA_URI}/views/{virtual_lab_id}/{str(project_id)}",
-                headers=get_client_headers(),
-            )
-        data = nexus_views.json()
-
-        print("ᦨ #  test_project_creation.py:153 #  data:", data)
-
-        print(
-            "ᦨ #  test_project_creation.py:150 #  url",
-            f"{settings.NEXUS_DELTA_URI}/views/{virtual_lab_id}/{str(project_id)}",
-        )
-        print(
-            "ᦨ #  test_project_creation.py:151 #  get_client_headers:",
-            get_client_headers(),
-        )
-        print("ᦨ #  test_project_creation.py:153 #  data:", data.get("_results"))
-
-        agg_view = [
-            view
-            for view in data.get("_results")
-            if sorted(view.get("@type")) == sorted(AGGREGATE_ELASTIC_SEARCH_VIEW)
-        ]
-        return len(agg_view) == 1
-
-    # Nexus takes some time to return the created views that's why wait until is needed here
-    aggregate_view_created = await wait_until(views_created, 10, 0.25)
-    assert aggregate_view_created is True
