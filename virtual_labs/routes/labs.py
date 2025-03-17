40f9b9966cfcde86ef4ff7e046a522228aba93e8
Bilal MEDDAH <b_meddah@esi.dz>
2025-03-17 09:45:53 +0100

    allow creation of multiple vlabs (#39)

--- virtual_labs/routes/labs.py
+++ virtual_labs/routes/labs.py
@@ -10,7 +10,11 @@ from virtual_labs.core.authorization import (
     verify_vlab_write,
 )
 from virtual_labs.core.types import UserRoleEnum, VliAppResponse
-from virtual_labs.domain.common import LabListWithPending
+from virtual_labs.domain.common import (
+    LabListWithPending,
+    PageParams,
+    PaginatedResultsResponse,
+)
 from virtual_labs.domain.email import (
     EmailVerificationPayload,
     InitiateEmailVerificationPayload,
@@ -46,17 +50,40 @@ PaginatedLabs = LabResponse[LabListWithPending[VirtualLabDetails]]
 router = APIRouter(prefix="/virtual-labs", tags=["Virtual Labs Endpoints"])
 
 
-@router.get("", response_model=LabResponse[LabListWithPending[VirtualLabDetails]])
+@router.get("", response_model=LabResponse[PaginatedResultsResponse[VirtualLabDetails]])
 @verify_user_authenticated
 async def get_paginated_virtual_labs_for_user(
-    db: AsyncSession = Depends(default_session_factory),
+    page: int = 1,
+    size: int = 10,
+    session: AsyncSession = Depends(default_session_factory),
     auth: tuple[AuthUser, str] = Depends(a_verify_jwt),
-) -> LabResponse[LabListWithPending[VirtualLabDetails | None]]:
+) -> LabResponse[PaginatedResultsResponse[VirtualLabDetails]]:
     return LabResponse(
         message="List of user virtual lab and pending labs from invites",
         data=await usecases.list_user_virtual_labs(
-            db,
+            session=session,
             auth=auth,
+            page_params=PageParams(page=page, size=size),
+        ),
+    )
+
+
+@router.get(
+    "/pending", response_model=LabResponse[PaginatedResultsResponse[VirtualLabDetails]]
+)
+@verify_user_authenticated
+async def get_pending_virtual_labs_for_user(
+    page: int = 1,
+    size: int = 10,
+    session: AsyncSession = Depends(default_session_factory),
+    auth: tuple[AuthUser, str] = Depends(a_verify_jwt),
+) -> LabResponse[PaginatedResultsResponse[VirtualLabDetails]]:
+    return LabResponse(
+        message="List of user pending virtual labs from invites",
+        data=await usecases.list_user_pending_virtual_labs(
+            session=session,
+            auth=auth,
+            page_params=PageParams(page=page, size=size),
         ),
     )
 
@@ -204,7 +231,7 @@ async def invite_user_to_virtual_lab(
     virtual_lab_id: UUID4,
     invite_details: AddUser,
     session: AsyncSession = Depends(default_session_factory),
-    auth: tuple[AuthUser, str] = Depends(verify_jwt),
+    auth: tuple[AuthUser, str] = Depends(a_verify_jwt),
 ) -> LabResponse[InviteSent]:
     invite_id = await usecases.invite_user_to_lab(
         virtual_lab_id,
