9de8f2578832d8af6241eaa904661409ec888a83
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2025-03-21 09:08:44 +0100

    Add discount usecase balance prop for account creation

--- virtual_labs/external/accounting/__init__.py
+++ virtual_labs/external/accounting/__init__.py
@@ -2,7 +2,8 @@ from virtual_labs.infrastructure.settings import settings
 
 from .assign_project_budget import assign_project_budget
 from .create_project_account import create_project_account
-from .create_virtual_lab_acount import create_virtual_lab_account
+from .create_virtual_lab_account import create_virtual_lab_account
+from .create_virtual_lab_discount import create_virtual_lab_discount
 from .get_project_balance import get_project_balance
 from .get_project_reports import get_project_reports
 from .get_virtual_lab_balance import get_virtual_lab_balance
@@ -17,6 +18,7 @@ __all__ = [
     "assign_project_budget",
     "create_project_account",
     "create_virtual_lab_account",
+    "create_virtual_lab_discount",
     "get_project_balance",
     "get_project_reports",
     "get_virtual_lab_balance",
--- virtual_labs/usecases/accounting/create_virtual_lab_account.py
+++ virtual_labs/usecases/accounting/create_virtual_lab_account.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from pydantic import UUID4
 
 import virtual_labs.external.accounting as accounting_service
@@ -5,10 +7,8 @@ from virtual_labs.external.accounting.models import VlabAccountCreationResponse
 
 
 async def create_virtual_lab_account(
-    virtual_lab_id: UUID4,
-    name: str,
+    virtual_lab_id: UUID4, name: str, balance: Decimal = Decimal(0)
 ) -> VlabAccountCreationResponse:
     return await accounting_service.create_virtual_lab_account(
-        virtual_lab_id=virtual_lab_id,
-        name=name,
+        virtual_lab_id=virtual_lab_id, name=name, balance=balance
     )
