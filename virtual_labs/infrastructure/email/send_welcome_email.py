c663cd296f498067e98097419ad921ce9d0a78d8
Tolokoban <contact@tolokoban.org>
2025-05-19 14:10:42 +0200

    Send a welcome email to the user that created a virtual lab (#93)

---
+++ virtual_labs/infrastructure/email/send_welcome_email.py
@@ -0,0 +1,22 @@
+from fastapi_mail import FastMail, MessageSchema, MessageType
+from loguru import logger
+
+from virtual_labs.infrastructure.email.config import email_config
+
+
+async def send_welcome_email(recipient: str) -> str:
+    try:
+        message = MessageSchema(
+            subject="Start your Simulation Journey!",
+            recipients=[recipient],
+            body="",
+            subtype=MessageType.html,
+            template_body={},
+        )
+        fm = FastMail(email_config)
+        await fm.send_message(message, template_name="welcome.html")
+        logger.info(f"A welcome email has been sent to {recipient}")
+        return f"email sent successfully to {recipient}"
+    except Exception:
+        logger.warning(f"Unable to send a welcome email to {recipient}!")
+        return f"email has not been sent to {recipient}"
