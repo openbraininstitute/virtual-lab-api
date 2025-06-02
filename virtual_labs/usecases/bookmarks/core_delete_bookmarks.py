e20a514b788dda5be009ef23e1de186cbcd52827
Bilal MEDDAH <b_meddah@esi.dz>
2025-06-02 12:32:26 +0200

    Revert "Revert "add entitycore entity id library bookmark endpoints (#82)" (#98)"
    This reverts commit de0a09a02898b9feae261cdc904408ae7f272a52.

--- virtual_labs/usecases/bookmarks/core_delete_bookmarks.py
+++ virtual_labs/usecases/bookmarks/core_delete_bookmarks.py
@@ -1,25 +1,26 @@
 from http import HTTPStatus
+from typing import List
 
-from loguru import logger
 from pydantic import UUID4
 from sqlalchemy.exc import NoResultFound, SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from virtual_labs.core.exceptions.api_error import VliError, VliErrorCode
-from virtual_labs.domain.bookmark import BookmarkCategory
+from virtual_labs.domain.bookmark import DeleteBookmarkIn
 from virtual_labs.repositories.bookmark_repo import BookmarkMutationRepository
 
 
-async def delete_bookmark(
-    db: AsyncSession, project_id: UUID4, resource_id: str, category: BookmarkCategory
+async def core_delete_bookmarks(
+    db: AsyncSession,
+    project_id: UUID4,
+    bookmarks: List[DeleteBookmarkIn],
 ) -> None:
     repo = BookmarkMutationRepository(db)
 
     try:
-        await repo.delete_bookmark_by_params(
+        await repo.delete_bookmarks_bulk(
             project_id=project_id,
-            resource_id=resource_id,
-            category=category.value,
+            bookmarks_to_delete=bookmarks,
         )
 
     except NoResultFound as error:
@@ -30,9 +31,6 @@ async def delete_bookmark(
             details=str(error),
         )
     except SQLAlchemyError as error:
-        logger.error(
-            f"DB error during deleting bookmark for resource {resource_id} - {category} from project {project_id}: ({error})"
-        )
         raise VliError(
             message="The bookmark could not be deleted",
             error_code=VliErrorCode.DATABASE_ERROR,
@@ -40,9 +38,6 @@ async def delete_bookmark(
             details=str(error),
         )
     except Exception as error:
-        logger.exception(
-            f"Error during deleting bookmark for {resource_id} - {category} from project {project_id}: ({error})"
-        )
         raise VliError(
             error_code=VliErrorCode.SERVER_ERROR,
             http_status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
