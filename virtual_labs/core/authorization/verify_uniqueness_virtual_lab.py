13f28bdb8731e890fd292629410efe5b0f07253e
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2026-03-25 11:07:11 +0100

    feedback

--- virtual_labs/core/authorization/verify_uniqueness_virtual_lab.py
+++ virtual_labs/core/authorization/verify_uniqueness_virtual_lab.py
@@ -7,13 +7,9 @@ from virtual_labs.core.exceptions.api_error import VliError, VliErrorCode
 from virtual_labs.infrastructure.db.config import default_session_factory
 from virtual_labs.infrastructure.kc.auth import verify_jwt
 from virtual_labs.infrastructure.kc.models import AuthUser
+from virtual_labs.infrastructure.settings import settings
 from virtual_labs.repositories.labs import get_user_virtual_lab
 from virtual_labs.shared.utils.auth import get_user_id_from_auth
-
-MULTIPLE_VLABS_ALLOWED_USER_IDS = [
-    "16588c8b-ec88-4a49-a413-a0bb3a7b8541"  # Staging
-    "a713cff1-d67d-4f3f-9c28-a92fae3ddf30"  # Prod
-]
 
 
 async def verify_uniq_virtual_lab(
@@ -23,7 +19,7 @@ async def verify_uniq_virtual_lab(
     """Dependency that raises if the authenticated user already owns a virtual lab."""
     owner_id = get_user_id_from_auth(auth)
 
-    if (owner_id) in MULTIPLE_VLABS_ALLOWED_USER_IDS:
+    if owner_id == settings.MULTIPLE_VLABS_ALLOWED_USER_IDS:
         return
 
     existing = await get_user_virtual_lab(db=db, owner_id=owner_id)
