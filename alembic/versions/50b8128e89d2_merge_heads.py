c309a582d44d9662e5c3b7e25747c798b8b9e6ab
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2026-06-23 12:29:44 +0200

    format

--- alembic/versions/50b8128e89d2_merge_heads.py
+++ alembic/versions/50b8128e89d2_merge_heads.py
@@ -5,15 +5,13 @@ Revises: 96c83d63c85e, e7b2c4a91f36
 Create Date: 2026-06-23 12:28:31.979654
 
 """
-from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
+from typing import Sequence, Union
 
 
 # revision identifiers, used by Alembic.
-revision: str = '50b8128e89d2'
-down_revision: Union[str, None] = ('96c83d63c85e', 'e7b2c4a91f36')
+revision: str = "50b8128e89d2"
+down_revision: Union[str, None] = ("96c83d63c85e", "e7b2c4a91f36")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
