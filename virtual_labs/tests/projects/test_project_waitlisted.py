3b76dd56fdaf065f50962ad73773cf873ab92920
Gil Arturo Barrios del Villar <gilarturo.barriosdelvillar@openbraininstitute.org>
2026-07-23 10:59:40 +0200

    fix test

--- virtual_labs/tests/projects/test_project_waitlisted.py
+++ virtual_labs/tests/projects/test_project_waitlisted.py
@@ -4,9 +4,9 @@ from uuid import UUID
 import pytest
 import pytest_asyncio
 from httpx import AsyncClient, Response
-from sqlalchemy import update
+from sqlalchemy import select, update
 
-from virtual_labs.infrastructure.db.models import Project
+from virtual_labs.infrastructure.db.models import Project, VirtualLab
 from virtual_labs.infrastructure.kc.config import KeycloakRealm
 from virtual_labs.tests.utils import (
     get_headers,
@@ -33,21 +33,28 @@ async def mock_waitlisted_project(
     assert group_id_or_none is not None
     group_id: str = group_id_or_none
 
+    vlab_member_group_id: str
     async with session_context_factory() as session:
         await session.execute(
             update(Project)
             .where(Project.id == UUID(project_id))
             .values(waitlisted_group_id=group_id)
         )
+        result = await session.execute(
+            select(VirtualLab.member_group_id).where(VirtualLab.id == UUID(virtual_lab_id))
+        )
+        vlab_member_group_id = str(result.scalar_one())
         await session.commit()
 
     waitlisted_headers = get_headers("test-1")
     user_id = str(await get_user_id_from_test_auth(waitlisted_headers["Authorization"]))
     KeycloakRealm.group_user_add(user_id=user_id, group_id=group_id)
+    KeycloakRealm.group_user_add(user_id=user_id, group_id=vlab_member_group_id)
 
     yield project_id, virtual_lab_id, waitlisted_headers
 
     KeycloakRealm.group_user_remove(user_id=user_id, group_id=group_id)
+    KeycloakRealm.group_user_remove(user_id=user_id, group_id=vlab_member_group_id)
 
 
 @pytest.mark.asyncio
