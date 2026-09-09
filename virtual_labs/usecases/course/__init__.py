29253fcdb9535fe35b6b9387059916ef896e2cba
Pavlo Getta <pavlo.getta@openbraininstitute.org>
2026-09-09 13:46:16 +0200

    Revert "Admin endpoint to apply discount to an existing course. (#205)" (#206)
    This reverts commit 57c6d7d698f7eeff4072762676a1e39f41d0be74.

--- virtual_labs/usecases/course/__init__.py
+++ virtual_labs/usecases/course/__init__.py
@@ -1,6 +1,5 @@
 from virtual_labs.usecases.course.activate_enrolment import activate_enrolment
 from virtual_labs.usecases.course.activate_enrolments import activate_enrolments
-from virtual_labs.usecases.course.apply_course_discount import apply_course_discount
 from virtual_labs.usecases.course.delete_course import delete_course
 from virtual_labs.usecases.course.assign_seats import assign_seats
 from virtual_labs.usecases.course.claim_enrolment import claim_enrolment
@@ -21,7 +20,6 @@ from virtual_labs.usecases.course.update_course_status import (
 __all__ = [
     "activate_enrolment",
     "activate_enrolments",
-    "apply_course_discount",
     "delete_course",
     "assign_seats",
     "claim_enrolment",
